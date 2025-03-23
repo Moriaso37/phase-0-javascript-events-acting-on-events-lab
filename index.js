adfe3617b24be34b56639934d180fc739ed0069f
moriaso <muriasosalaon@gmail.com>
2025-03-23 16:42:56 +0300

    event

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`; // Move left by 10px
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`; // Move right by 10px
+    }
+  }
